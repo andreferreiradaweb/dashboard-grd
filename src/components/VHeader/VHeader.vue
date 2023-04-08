f2d4658d17c9e17e3c6dfb46a12a7d6ea7941567
Andre <andrebmx789@gmai.com>
2023-04-08 04:44:49 -0300

    feat: first commit

---
+++ src/components/VHeader/VHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VHeader',
+  components: {
+  },
+  data () {
+    return {
+      columns: '',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles.scss';
+</style>
---
+++ src/pages/home/VHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/VHeader/VHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles.scss';
+</style>
